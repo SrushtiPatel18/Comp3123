b5680dd023fe48ada44eaf87fc57f278c1439cc2
Srushti Patel <srushtimpatel007@gmail.com>
2025-10-20 23:57:45 -0400

    Lab Week 9 Exercise

--- config/db.js
+++
@@ -1,4 +0,0 @@
-mongoose.connect("mongodb+srv://<srushtimaheshkumarpatel_db_user:QzRYQwUqDipgbP4u@Cluster0.mongodb.net/lab_exercise6_notes_db?retryWrites=true&w=majority&appName=<appName>", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
